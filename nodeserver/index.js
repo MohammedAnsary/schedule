de99885385733e535e4b5540fc9b01c47f389b87
MohammedAnsary <a.n.s.a.r.y@hotmail.com>
2017-01-16 22:14:57 +0200

    Added CSV parsing

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -37,64 +37,59 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors());
 
 app.post('/', (req, res) => {
-	console.log(req.body);
-	var childProcess = cp.spawn('swipl');
 
-	childProcess.stdin.write('1 + 2');
-
-	var stdin = process.openStdin();
-	stdin.addListener('data', (data) => {
-		childProcess.stdin.write(';\n');
-		console('haaah');
-	});
-
-	childProcess.stderr.on('data', (data) => {
-		console.log(`${data}`);
-	});
-
-	childProcess.stdout.on('data', (data) => {
-		console.log(`${data}`);
-	});
-
-	res.send('lol');
 });
-app.locals.test = "omar"
-
-  app.listen(8888);
 
 
+app.listen(8888);
 
 
 
+function parse(entries) {
+	var parsedTimings = new Array();
+	var parsedGroups = new Array();
+	var courseNames = new Array();
+	var parsedGroupNames = new Array();
+	var groupMap = new Array();
+	var groupSoFar = 0;
+	for(var i = 0; i < entries.length;) {
+		var timingsCourse = new Array();
+		var groupsCourse = new Array();
+		var course = entries[i][0];
+		courseNames.push(course);
+		for(var j = i; j < entries.length && course == entries[j][0];) {
+			var type = entries[j][4];
+			for(var k = j; k < entries.length && type == entries[k][4];) {
+				var group = entries[k][5];
+				var idx = timingsCourse.length;
+				if(!groupMap[group]) {
+					groupSoFar++;
+					groupMap[group] = groupSoFar;
+				}
+				for(var l = k; l < entries.length && group == entries[l][5]; l++) {
+					if(!timingsCourse[idx])
+						timingsCourse[idx] = new Array();
+					if(!groupsCourse[idx]) {
+						groupsCourse[idx] = new Array();
+						parsedGroupNames[idx] = new Array();
+					}
+					timingsCourse[idx].push(entries[l][3]);
+					if(type != 'Lecture') {
+						groupsCourse[idx].push(groupMap[group]);
+						parsedGroupNames[idx].push(course + ' ' + group);
+					}
+				}
+				k = l;
+			}
+			j = k;
+		}
+		i = j;
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var childProcess = cp.spawn('swipl', ['mini3.pl']);
-//
-// childProcess.stdout.setEncoding('utf8');
-//
-// var data_line = '';
-// childProcess.stdin.write(query);
-// var stdin = process.openStdin();
-// stdin.addListener("data", function(d) {
-//     childProcess.stdin.write(';\n');
-// });
-// childProcess.stdout.on("data", function(data) {
-//   data_line += data;
-//   if (data_line.split('=').length - 1 == 2) {
-//     console.log(data_line);
-//     data_line = '';
-//     var stdin = process.openStdin();
-//   }
-// });
+function csvToArray (csv) {
+    rows  = csv.split("\n");
+    return rows.map(function (row) {
+    	return row.split(",");
+    });
+};
