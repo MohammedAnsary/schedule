68ae9b468ac6cf87963714a0af1e3c5ab028ab12
Omar Essam <o.essameldin@gmail.com>
2017-01-16 23:13:50 +0200

    add next function and routes

--- nodeserver/routes/main.js
+++ nodeserver/routes/main.js
@@ -7,14 +7,22 @@ var index = require('../connection')
 //   res.render('main', { title: 'Express' });
 // });
 
-router.get('/plquery', function(req, res) {
+router.get('/ask', function(req, res) {
     // index.ask("assign_sched(CoursesSched,GroupsSched,[[[2,10], [8,3], [13,25]]],[[[], [1,2], [1,2]]], 1, 3, 2 )");
-    index.ask("X is 1, B = X", (result) => {
+    index.ask("assign_sched(CoursesSched,GroupsSched,[[[2,10], [8,3], [13,25]]],[[[], [1,2], [1,2]]], 1, 3, 2 )", (result) => {
         res.send({
             response: result
         });
     });
+});
 
+router.get('/next', function(req, res) {
+    // index.ask("assign_sched(CoursesSched,GroupsSched,[[[2,10], [8,3], [13,25]]],[[[], [1,2], [1,2]]], 1, 3, 2 )");
+    index.next((result) => {
+        res.send({
+            response: result
+        });
+    });
 });
 
 module.exports = router;
