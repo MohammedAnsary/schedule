079ca1b84aa4020d2b947d17d1b010a2ad0b3762
Omar Essam <o.essameldin@gmail.com>
2017-01-17 21:38:43 +0200

    add real prolog request

--- nodeserver/routes/main.js
+++ nodeserver/routes/main.js
@@ -19,14 +19,22 @@ router.get('/ask', function(req, res) {
 
 });
 
-router.get('/next/:chunk', function(req, res) {
-    console.log(req.params.chunk);
-    index.ask("assign_sched(CoursesSched,GroupsSched,[[[2,10], [8,3], [13,25]]],[[[], [1,2], [1,2]]], 1, 3, 2 )", parseInt(req.params.chunk), function(result) {
+// router.get('/next/:chunk', function(req, res) {
+//     console.log(req.params.chunk);
+//     index.ask("assign_sched(CoursesSched,GroupsSched,[[[2,10], [8,3], [13,25]]],[[[], [1,2], [1,2]]], 1, 3, 2 )", parseInt(req.params.chunk), function(result) {
+//             return res.send({
+//                 response: result
+//             });
+//     });
+// });
+app.post('/next', function(req, res) {
+    var variables = parserequest(req.body);
+index.ask("choose_subjects_and_generate_schedule("+variables.allowedCreditHours+","+variables.ObligatoryCourses+","+variables.coursesCreditHours+","+variables.probation+","+variables.history+","+variables.prerequisites+","+variables.coursesTimings+","+variables.coursesGroups+","+variables.slotsNames+","+variables.condensed+","+variables.totalCreditHours+","+variables.newCoursesSched+","+variables.groupsSched+")", parseInt(req.body.chunk), function(result) {
             return res.send({
                 response: result
             });
     });
-
+    res.send(user_id + ' ' + token + ' ' + geo);
 });
 
 module.exports = router;
